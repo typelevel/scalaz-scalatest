26a09ff33ae16da963eef2919a427b291655c9db
Brendan W. McAdams <bwmcadams@evilmonkeylabs.com>
2014-11-15 11:45:12 -0800

    Test for DisjunctionValues (i.e. \/.value). Shamelessly swiped from Scalatest

---
+++ src/test/scala/DisjunctionValuesSpec.scala
@@ -0,0 +1,33 @@
+package net.evilmonkeylabs.scalatest
+
+import org.scalatest.{FunSpec, Matchers}
+import Matchers._
+import org.scalatest.OptionValues._
+import org.scalatest.exceptions.TestFailedException
+import scalaz.{ -\/, \/, \/- }
+
+class DisjunctionValuesSpec extends FunSpec with Util {
+  import DisjunctionValues._
+
+  describe("value on Disjunction") {
+
+
+    it("should return the value inside a disjunction (\\/) if that disjunction is \\/- (right)") {
+      val r: String \/ String = \/.right(thisRecord)
+      r.value should === (thisRecord)
+    }
+
+    it("should throw TestFailedException if that disjunction (\\/) is -\\/ (left) ") {
+
+      val r: String \/ String = \/.left(thisTobacconist)
+      val caught =
+        evaluating {
+          r.value should === (thisRecord)
+        } should produce [TestFailedException]
+      caught.failedCodeLineNumber.value should equal (thisLineNumber - 2)
+      caught.failedCodeFileName.value should be ("DisjunctionValuesSpec.scala")
+    }
+  }
+}
+
+
---
+++ src/test/scala/Util.scala
@@ -0,0 +1,21 @@
+
+package net.evilmonkeylabs.scalatest
+
+trait Util {
+
+  val thisRecord = "I will not buy this record, it is scratched."
+  val thisTobacconist = "Ah! I will not buy this tobacconist's, it is scratched."
+  val hovercraft = "Yes, cigarettes. My hovercraft is full of eels."
+  /**
+   * Shamelessly swiped from Scalatest.
+   */
+  def thisLineNumber = {
+    val st = Thread.currentThread.getStackTrace
+
+    if (!st(2).getMethodName.contains("thisLineNumber"))
+      st(2).getLineNumber
+    else
+      st(3).getLineNumber
+  }
+
+}
